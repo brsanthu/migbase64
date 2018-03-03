173a08773a8d50401e9030dfa7b338b3909808ae
Miha_x64 <mihlo1996@rambler.ru>
2018-03-03 12:04:58 +0300

    Fixed test

--- src/test/java/com/migcomponents/migbase64/CompareTest.java
+++ src/test/java/com/migcomponents/migbase64/CompareTest.java
@@ -1,22 +1,14 @@
 package com.migcomponents.migbase64;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Random;
 
-import com.migcomponents.migbase64.Base64;
-import com.migcomponents.migbase64.Base64IO;
-
 import junit.framework.TestCase;
 
-//import org.apache.commons.io.FileUtils;
-//import org.apache.commons.io.IOUtils;
+import static org.junit.Assert.assertArrayEquals;
 
-
-/* NOTE! Test commented out by Mikael Grev since it has a dependency on Apache Commons.*/
 
 /**
  * @author shamilbi shamilbi@users.sourceforge.net
@@ -37,8 +29,7 @@ public class CompareTest extends TestCase
 
     private final File[] files = new File[] { inFile, migbase64File, streamFile };
 
-    protected void setUp() throws Exception
-    {
+    protected void setUp() throws Exception {
         testDir.mkdirs();
         FileOutputStream out = new FileOutputStream(inFile);
         Random random = new Random(new Date().getTime());
@@ -52,8 +43,7 @@ public class CompareTest extends TestCase
         out.close();
     }
 
-    protected void tearDown() throws Exception
-    {
+    protected void tearDown() {
         for (int i = 0; i < files.length; i++)
         {
             files[i].delete();
@@ -66,37 +56,61 @@ public class CompareTest extends TestCase
      * @see Base64#encodeToByte(byte[], boolean)
      * @throws IOException
      */
-    public void test01() throws IOException
-    {
-//        FileInputStream in = new FileInputStream(inFile);
-//        FileOutputStream out = new FileOutputStream(streamFile);
-//        try
-//        {
-//            Base64IO.encode(in, out, false);
-//        } finally
-//        {
-//            in.close();
-//            out.close();
-//        }
-//
-//        final byte[] inBytes = FileUtils.readFileToByteArray(inFile);
-//        final byte[] migBase64 = Base64.encodeToByte(inBytes, false);
-//        FileUtils.writeByteArrayToFile(migbase64File, migBase64);
-//
-//        FileInputStream in1 = new FileInputStream(streamFile);
-//        FileInputStream in2 = new FileInputStream(migbase64File);
-//        try
-//        {
-//            assertTrue(IOUtils.contentEquals(in1, in2));
-//        } finally
-//        {
-//            in1.close();
-//            in2.close();
-//        }
-//
-//        final byte[] encodedBytes = FileUtils.readFileToByteArray(migbase64File);
-//        final byte[] decodedBytes = Base64.decode(encodedBytes);
-//        assertTrue(java.util.Arrays.equals(inBytes, decodedBytes));
+    public void test01() throws IOException {
+        FileInputStream in = new FileInputStream(inFile);
+        FileOutputStream out = new FileOutputStream(streamFile);
+        try
+        {
+            Base64IO.encode(in, out, false);
+        } finally
+        {
+            in.close();
+            out.close();
+        }
+
+        final byte[] inBytes = readFileToByteArray(inFile);
+        final byte[] migBase64 = Base64.encodeToByte(inBytes, false);
+        writeByteArrayToFile(migbase64File, migBase64);
+
+        FileInputStream in1 = new FileInputStream(streamFile);
+        FileInputStream in2 = new FileInputStream(migbase64File);
+        try
+        {
+            assertTrue(contentEquals(in1, in2));
+        } finally
+        {
+            in1.close();
+            in2.close();
+        }
+
+        final byte[] encodedBytes = readFileToByteArray(migbase64File);
+        final byte[] decodedBytes = Base64.decode(encodedBytes);
+        assertArrayEquals(inBytes, decodedBytes);
+    }
+
+    private static byte[] readFileToByteArray(File file) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(file, "r");
+        byte[] bytes = new byte[(int) raf.length()];
+        raf.readFully(bytes);
+        return bytes;
+    }
+
+    private static void writeByteArrayToFile(File file, byte[] bytes) throws IOException {
+        new RandomAccessFile(file, "rw").write(bytes);
+    }
+
+    private static boolean contentEquals(InputStream in1, InputStream in2) throws IOException {
+        byte[] b1 = new byte[1024];
+        byte[] b2 = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = in1.read(b1)) > 0) {
+            if (bytesRead != in2.read(b2))
+                return false;
+
+            if (!Arrays.equals(b1, b2))
+                return false;
+        }
+        return true;
     }
 
 }
